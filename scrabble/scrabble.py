af2eeeb86e09dcb65f6c4f68d2987348d3b897b0
riddj <joshridderjosh@gmail.com>
2023-04-04 16:05:28 -0400

    dummy Tile class

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -10,7 +10,23 @@ class Board():
     pass
 
 class Tile():
-    pass
+
+    # First value is point value, second value is number of tiles in a game
+    LETTERS = {"A":(1,9), "B":(3,2), "C":(3,2), "D":(2,4), "E":(1,12), "F":(4,2), "G":(2,3), "H":(4,2), "I":(1,9), 
+               "J":(8,1), "K":(5,1), "L":(1,4), "M":(3,2), "N":(1,6), "O":(1,8), "P":(3,2), "Q":(10,1), "R":(1,6), 
+               "S":(1,4), "T":(1,6), "U":(1,4), "V":(4,2), "W":(4,2), "X":(8,1), "Y":(4,2), "Z":(10,1), " ":(0,2)}
+
+    def __init__(self, letter):
+        self._letter = letter
+
+    def get_point_value(self):
+        return Tile.LETTERS[self._letter[0]]
+    
+    def get_letter_frequency(self):
+        return Tile.LETTERS[self._letter[1]]
+    
+    def get_letter_frequency(self, letter):
+        return Tile.LETTERS[letter[1]]
 
 class Game():
     
