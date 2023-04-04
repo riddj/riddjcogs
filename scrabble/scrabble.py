39cc27d18f600d057fc3cf3d72fbc04f94c69601
riddj <joshridderjosh@gmail.com>
2023-04-04 15:17:08 -0400

    dummy cog

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,13 +1,37 @@
+import logging
 from redbot.core import commands
 
+log = logging.getLogger("red.riddj.scrabble")
+
+class Player():
+    pass
+
+class Board():
+    pass
+
+class Tile():
+    pass
+
+class Game():
+    pass
+
 class Scrabble(commands.Cog):
-    """My custom cog"""
+    """
+    Play Scrabble with your friends over discord!
+    """
 
     def __init__(self, bot):
         self.bot = bot
+        self.games = []
+        self.MAXPLAYERS = 4
+        self.MAXDEADTIME = 3600
 
-    @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def red_delete_data_for_user(self, **kwargs):
+        """ Nothing to delete. """
+        return
+
+    @commands.group()
+    async def scrabble(self, ctx):
+        """ Create or join a game of Scrabble. """
+        if ctx.invoked_subcommand is None:
+            ctx.send(f"Create or join a game of scrabble using {ctx.prefix}scrabble new or {ctx.prefix}scrabble join.")
