600685f1fad5b172deff3ead5219b462384576a5
riddj <joshridderjosh@gmail.com>
2023-06-16 16:58:13 -0400

    update scrabble init

--- scrabble/__init__.py
+++ scrabble/__init__.py
@@ -6,6 +6,6 @@ from .scrabble import Scrabble
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
-def setup(bot):
+async def setup(bot):
     """ Setup Scrabble cog. """
-    bot.add_cog(Scrabble(bot))
+    await bot.add_cog(Scrabble(bot))
